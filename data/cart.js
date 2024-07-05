2db9d3f484a6267cf7375c5b9a5c02430175ea10
Yaman <yyaman949@gmail.com>
2024-07-05 22:25:59 +0300

    checkout generate

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,21 @@
-export const cart = [{}];
+export const cart = [{
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    quantity: 6
+}];
 
 export function addToCart(productName, productQuantity) {
     let found = false;
     cart.forEach(item => {
-        if (item.productName === productName) {
-            item.productQuantity += productQuantity;
+        if (item.name === productName) {
+            item.quantity += productQuantity;
             found = true;
         }
     });
 
     if (!found) {
         cart.push({
-            productName,
-            productQuantity
+            name: productName,
+            quantity: productQuantity
         });
-    };
+    }
 }
